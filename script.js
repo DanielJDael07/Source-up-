541005a1da94012c20147aad93093425904fe6c1
Daniel Dael <danieldael7777@gmail.com>
2023-01-29 17:27:26 +0800

    adding plug ins to all web pages and js restructuring

--- script.js
+++ script.js
@@ -66,7 +66,30 @@ fetch(form.action, {
 // End of connecting sheetdb API
 
 
-      
+// Messenger chat box plug in
+
+  var chatbox = document.getElementById('fb-customer-chat');
+  chatbox.setAttribute("page_id", "111303801868178");
+  chatbox.setAttribute("attribution", "biz_inbox");
+
+
+// Your SDK code
+
+  window.fbAsyncInit = function() {
+    FB.init({
+      xfbml            : true,
+      version          : 'v15.0'
+    });
+  };
+
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+  
    
    
    
